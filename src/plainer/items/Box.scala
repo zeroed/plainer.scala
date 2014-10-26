608c8c3bb9444529b51ac829adfa7fcfa237fdb5
Edoardo Rossi <edd.rossi@gmail.com>
2014-10-26 16:53:40 +0100

    Huge commit
    I've no excuse.
    :scream_cat:

--- src/plainer/items/Box.scala
+++ src/plainer/items/Box.scala
@@ -9,30 +9,36 @@ import plainer.traits.Cache
  * @author eddie
  *
  */
-class Box(key: String, paramValue: Int) extends Cache[String, Int] {
-  private var value: Option[Int] = Some(paramValue)
+class Box(val key: String, var value: Option[Int]) extends Cache[String, Int] {
+  //private var value: Option[Int] = Some(paramValue)
 
+  def this(key: String, value: Int) {
+    this(key, Option[Int](value))
+  }
+  
   def get(paramKey: String): Option[Int] = {
-    if (key.equals(paramKey)) value 
+    if (key.equals(paramKey)) value
     else None
   }
 
-  def put(paramKey: String, paramValue: Int) = {
-    println("putting %d in %s".format(paramValue, paramKey))
-    if (key.equals(paramKey)) {
-      value = Some(paramValue)
+  def put(key: String, paramValue: Int) = {
+    println("I have (this) %s and your are putting %d in %s".format(this.key, paramValue, key))
+    if (this.key.equals(key)) {
+      value = Option[Int](paramValue)
       true
     } else false
   }
 
-  def delete(paramKey: String) = {
-    println("nulling %s".format(paramKey))
-    if (key.equals(paramKey)) {
+  def delete(key: String) = {
+    println("nulling %s".format(key))
+    if (this.key.equals(key)) {
       value = None: Option[Int]
       true
     } else false
   }
 
+  def unary_! : String = { "Not!" }
+  
   override def toString: String = {
     "I'm a Box: key %s, value %s".format(key, value)
   }
