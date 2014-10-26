8b212016971342d6fe9f2e65fa05b428207efa10
Edoardo Rossi <edd.rossi@gmail.com>
2014-10-26 16:58:23 +0100

    Queue implementation

---
+++ src/plainer/utils/queues.scala
@@ -0,0 +1,53 @@
+/**
+ *
+ */
+package plainer.utils
+
+/**
+ * @author eddie
+ *
+ */
+package queues {
+  
+  trait Queue[T] {
+    def head: T
+    def tail: Queue[T]
+    def enqueue(x: T): Queue[T]
+  }
+
+  object Queue {
+    
+    /*
+     * Buildme
+     */
+    def apply[T](xs: T*): Queue[T] =
+      new QueueImpl[T](xs.toList, Nil)
+
+    /*
+     * Everything below is private, yo.
+     */
+    private class QueueImpl[T](
+      private val leading: List[T],
+      private val trailing: List[T]
+    ) extends Queue[T] {
+      
+      def mirror = 
+        if (leading.isEmpty)
+          new QueueImpl(trailing.reverse, Nil)
+        else
+          this
+          
+      def head: T =
+        mirror.leading.head
+      
+      def tail: QueueImpl[T] = {
+        val q = mirror
+        new QueueImpl(q.leading.tail, q.trailing)
+      }
+      
+      def enqueue(x: T) =
+        new QueueImpl(leading, x :: trailing)
+      
+    }
+  }
+}
