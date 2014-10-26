40962bef90159e0fcb3c18dcd224382510f9dc16
Edoardo Rossi <edd.rossi@gmail.com>
2014-10-26 16:59:12 +0100

    Counter with Actors
    Just a placeholder now...

---
+++ src/plainer/external/SimpleCounter.scala
@@ -0,0 +1,36 @@
+package plainer.external
+
+//import akka.actor.{Actor, ActorSystem}
+//import akka.actor.Props
+//import akka.event.Logging
+//
+//class MyActor extends Actor {
+//  val log = Logging(context.system, this)
+//  def receive = {
+//    case "test" => log.info("received test")
+//    case _ => log.info("received unknown message")
+//  }
+//}
+//
+//class SimpleCounter extends Actor {
+//  var count = 0
+//  def receive = {
+//    case "add" => count += 1
+//    case "echo" => println(count)
+//    case _     => // do nothing
+//  }
+//}
+//
+//object TryActors extends App {
+//  def doStuff = {
+//    val system = ActorSystem("system")
+//    val counter = system.actorOf(Props(new SimpleCounter), "counter")
+//    
+//    counter ! "add"
+//    counter ! "add"
+//    counter ! "add"
+//    counter ! "echo" // 3
+//  }
+//  doStuff
+//}
+
